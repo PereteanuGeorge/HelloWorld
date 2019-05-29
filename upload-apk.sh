4fc2fefef03adcb45cb006fcf7992610b7cc8301
PereteanuGeorge <gp3917@ic.ac.uk>
2019-05-29 17:40:29 +0100

    Fmm 2

--- upload-apk.sh
+++ upload-apk.sh
@@ -1,4 +1,4 @@
-mkdir $HOME/buildApk/
+mkdir buildApk/
 cp -R app/build/outputs/apk/app-debug.apk $HOME/android/
 cd $HOME
 git config --global user.email "gp3917@ic.ac.uk"
@@ -6,10 +6,9 @@ git config --global user.name "PereteanuGeorge"
 
 git clone --quiet --branch=master  https://PereteanuGeorge:$GITHUB_API_KEY@github.com/PereteanuGeorge/HelloWorld  master > /dev/null
 
-cd master  cp -Rf $HOME/android/* .
+cd master  cp -Rf android/* .
 #add, commit and push files
 git add -f .
-git remote rm origin
 git remote add origin https://PereteanuGeorge:$GITHUB_API_KEY@github.com/PereteanuGeorge/HelloWorld.git
 git add -f .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed"
