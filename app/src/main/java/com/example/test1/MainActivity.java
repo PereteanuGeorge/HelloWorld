7f4cb8a9e068199abfff7e4dda0560bf54f7549d
PereteanuGeorge <gp3917@ic.ac.uk>
2019-05-30 14:01:31 +0100

    See changes

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Button btn = findViewById(R.id.button);
+        btn.setText("12");
     }
 
     @Override
